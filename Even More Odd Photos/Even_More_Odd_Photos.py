734868bd7a690dd9e8b07e7e42ba4fac809c8fc7
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-14 13:37:24 -0400

    Create Even_More_Odd_Photos.py

---
+++ Even More Odd Photos/Even_More_Odd_Photos.py
@@ -0,0 +1,85 @@
+p= int(input(''))
+ask = input('').split(' ')
+even = 0
+odd = 0
+l = []
+for y in ask:
+    sum1 = int(y)/2
+    if sum1==int(sum1):
+        even+=1
+    else:
+        odd+=1
+if odd==0 and even>0:
+    print(1)
+    exit()
+if odd>0 and even==0:
+    bool = False
+    for q in range(p):
+        if odd==1 and bool==True:
+            l[-2]+=2
+            odd-=1
+            l.pop(-1)
+            break
+        else:
+            if p!=sum(l):
+                if len(l)==0:
+                    l.append(2)
+                    odd-=2
+                    bool = False
+                elif l[-1]==1:
+                    l.append(2)
+                    odd-=2
+                    bool = False
+                elif l[-1]==2:
+                    l.append(1)
+                    odd-=1
+                    bool = True
+    print(len(l))
+    exit()
+if even>odd:
+    bool = False
+    for q in range(p):
+        if bool == False:
+            l.append(2)
+            even -=1
+            bool = True
+        elif bool == True:
+            if odd == 0:
+                break
+            else:
+                l.append(1)
+                odd -=1
+                bool = False
+    print(len(l))
+    exit()
+if even==odd:
+    print(even+odd)
+    exit()
+if odd>even:
+    bool = False
+    for q in range(p):
+        if odd==1 and bool:
+            l[-2]+=2
+            odd-=1
+            l.pop(-1)
+            break
+        else:
+            if len(l)==0:
+                l.append(2)
+                even-=1
+                bool = False
+            elif l[-1]==1:
+                l.append(2)
+                if even!=0:
+                    even-=1
+                else:
+                    odd-=2
+                bool = False
+            elif l[-1]==2:
+                l.append(1)
+                odd-=1
+                bool = True
+        if odd==0 and even==0:
+            break
+    print(len(l))
+    exit()
