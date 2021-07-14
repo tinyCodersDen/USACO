f536a1e9dded62695ce9542ca0545c5efa664b4e
tinyCoder <65143775+tinyCodersDen@users.noreply.github.com>
2021-07-14 13:25:43 -0400

    Create Do_You_Know_Your_ABCs.py

---
+++ Do You Know Your ABCs?/Do_You_Know_Your_ABCs.py
@@ -0,0 +1,9 @@
+ask = input("")
+list1 = ask.split(' ')
+list1 = [int(y) for y in list1]
+list1.sort()
+a,b = list1[0],list1[1]
+for x in list1:
+    if a+b in list1 and b+x in list1 and x+a in list1 and a+b+x in list1:
+        c = x
+print(a,b,c)
