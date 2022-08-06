0051c50e4a2548572f50bfb226829b194c60758a
Vihan <65143775+tinyCodersDen@users.noreply.github.com>
2022-08-06 12:08:00 -0400

    Create DiamondCollector.py

---
+++ Diamond Collector/DiamondCollector.py
@@ -0,0 +1,16 @@
+f = open('diamond.in','r')
+N,k  = list(map(int,f.readline().split(' ')))
+l = []
+for t in range(N):
+    l.append(int(f.readline()))
+s = 0
+for e,t in enumerate(l):
+    c = 1
+    for w,a in enumerate(l):
+        if w!=e and abs(t-a)<=k and t<=a:
+            c+=1
+    if c>s:
+        s = c
+print(s)
+w = open('diamond.out','w')
+w.write(str(s))
